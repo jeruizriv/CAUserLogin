9dfb76c799daf78ee4a8389053fd2b99b2cd6716
Jaime Ruiz Rivera <jaime.ruizrivera@mail.utoronto.ca>
2024-10-21 14:03:08 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets up the name.
+     * @param name the name to set up
+     */
+    void setCurrentUser(String name);
 }
